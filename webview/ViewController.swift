10de99058c89cd5d3aba86798beb07c43b38008c
Burak Mestan <burakmestan@gmail.com>
2019-09-17 10:43:35 +0300

    Initial Commit

---
+++ webview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview
+//
+//  Created by burak mestan on 17.09.2019.
+//  Copyright © 2019 burak mestan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
